fe1913ff1601a82ac7bed95938f5d8a336e7e7b2
Trent-Duncan <duncantr131@northwested.org>
2024-09-11 13:39:54 -0400

    Update paired-concat-practice.py

--- paired-concat-practice.py
+++ paired-concat-practice.py
@@ -9,20 +9,27 @@
 # Part 1
 # Create variables to store your first, middle, and last name
 # Have Python print a message that contains your concatenated full name, i.e., your combined first, middle and last names
-
-
+first_name= 'Trent '
+middle_name= 'neilson '
+last_name= 'Duncan '
+first_name_2 = ' Matthew '
+middle_name_2 = 'Wayne '
+last_name_2 = 'Hall '
+print(first_name + middle_name + last_name + "and" + first_name_2 + middle_name_2 + last_name_2)
 
 # Part 2
 # Assume you're building a Space Invaders game
 # Use concatenation to create and display a custom welcome message that includes the player's first name
-
-
+print ('welcome to my Space Invaders game Matthew!')
 
 # Part 3
 # Use concatenation to build and display a string that includes your first name, last name, and the year you were born
-
+birth_year=2008
+print(first_name + middle_name + last_name + "was born in the year " + str(birth_year) + ".")
 
 
 # Part 4
 # Use concatenation to create and display a sentence that says which country in the world
 # you would visit if money were no object
+country= ' nowhere'
+print("if i could go anywhere i'd go" + country)
